165ab4972515e91b20efb3d0a17b4a03b5847670
Kristina <kristina.milentijevic13@gmail.com>
2025-04-09 20:38:30 +0200

    Changes

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+// app.use(cors({
+//     origin: 'https://agrishop-react.onrender.com',  
+//     credentials: true 
+// }));
+
+const allowedOrigins = ['http://localhost:3000', 'https://agrishop-react.onrender.com'];
+
 app.use(cors({
-    origin: 'https://agrishop-react.onrender.com',  
-    credentials: true 
+  origin: function(origin, callback){
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // Dozvoli sve origene iz liste
+      callback(null, true);
+    } else {
+      // Ako origin nije u listi, blokiraj ga
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
+
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser());
